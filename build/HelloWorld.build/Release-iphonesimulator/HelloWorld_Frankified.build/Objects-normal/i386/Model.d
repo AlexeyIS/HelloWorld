32f2ffe25aee3e6db5fd7b988aa767cdfa988b62
Alexey Ledovskiy <alexey@concreteinteractive.com>
2012-09-14 16:12:45 -0700

    changes

--- build/HelloWorld.build/Release-iphonesimulator/HelloWorld_Frankified.build/Objects-normal/i386/Model.d
+++
@@ -1,2 +0,0 @@
-dependencies: /Users/Alex/Documents/HelloWorld/HelloWorld/Model.m \
-  /Users/Alex/Documents/HelloWorld/HelloWorld/Model.h
--- build/Release-iphonesimulator/HelloWorld copy.app/recording.rb
+++
@@ -1,5 +0,0 @@
-Around( '@record' ) do | scenario, block |
-  start_recording
-  block.call
-  stop_recording
-end
