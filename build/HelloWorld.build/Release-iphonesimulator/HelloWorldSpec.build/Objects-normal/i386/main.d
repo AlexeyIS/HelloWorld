29147dd4a49ddd70b526c20e325b500a2c0d72a7
Alexey Ledovskiy <alexey@concreteinteractive.com>
2012-09-14 12:27:51 -0700

    Don't sign project

---
+++ build/HelloWorld.build/Release-iphonesimulator/HelloWorldSpec.build/Objects-normal/i386/main.d
@@ -0,0 +1,7 @@
+dependencies: /Users/Alex/Documents/HelloWorld/HelloWorldSpec/main.m \
+  /Users/Alex/Documents/HelloWorld/HelloWorldSpec/Frameworks/Cedar-iOS.framework/Headers/Cedar-iOS.h \
+  /Users/Alex/Documents/HelloWorld/HelloWorldSpec/Frameworks/Cedar-iOS.framework/Headers/Cedar.h \
+  /Users/Alex/Documents/HelloWorld/HelloWorldSpec/Frameworks/Cedar-iOS.framework/Headers/CDRFunctions.h \
+  /Users/Alex/Documents/HelloWorld/HelloWorldSpec/Frameworks/Cedar-iOS.framework/Headers/CedarApplicationDelegate.h \
+  /Users/Alex/Documents/HelloWorld/HelloWorldSpec/Frameworks/Cedar-iOS.framework/Headers/CDRExampleReporterViewController.h \
+  /Users/Alex/Documents/HelloWorld/HelloWorldSpec/Frameworks/Cedar-iOS.framework/Headers/CDRExampleReporter.h
